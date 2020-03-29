8ecd69471507a75bdf6987c40a27746a659aabe7
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 18:08:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -71,7 +71,7 @@ def game_hash
           blocks: 7,
           slam_dunk: 2
         }, {
-          player_name: "Bismac Biyombo",
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
