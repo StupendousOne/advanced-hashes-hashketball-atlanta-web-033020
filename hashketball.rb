69368e683525bdc0bf14887fa1d66a1c8cc06978
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 17:26:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,28 @@
 def game_hash()
   final_hash = {
     home: {
-      team_name: "Home Team",
-      colors: [],
-      players: []
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "name",
+          number: 1000,
+          shoe: "Shoe",
+          points: 1000,
+          rebounds: 1000,
+          assists: 1000,
+          steals: 1000,
+          blocks: 1000,
+          slam_dunk: 1000
+        }
+        
+        
+        
+        
+        
+        
+        
+      ]
     },
     away: {
       team_name: "Away Team",
