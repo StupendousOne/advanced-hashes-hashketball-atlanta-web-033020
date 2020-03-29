1e155f2ed4aceda5e8fe8ae1823858d4f7702b25
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-29 17:20:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,11 +2,17 @@
 def game_hash()
   final_hash = {
     home: {
-      {team_name: "Home Team"},
-      
+      team_name: "Home Team",
+      colors: [],
+      players: []
+    },
+    away: {
+      team_name: "Away Team",
+      colors: [],
+      players: []
     }
-    
   }
+  
   final_hash  
 end
 
